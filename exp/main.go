8a6afee4f9694616fe84bf3f4fe26096264fbba0
MariumAZ <mariem.azzouz@dauphine.tn>
2021-12-02 10:07:51 +0100

    contextual context

---
+++ exp/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+type User struct {
+	Name string
+	Bio string
+	Age int
+}
+
+func main() {
+	// t pointer to template
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		// no easyv way out to recover
+		panic(err)
+	}
+	// create some data to put on our template
+    user := User{
+		Name : "Myriam",
+		Bio: `<script>alert("Haha you have been h4x0r3d!");</script>`,
+		Age: 123,
+	}
+
+	// execute template using this data
+	err = t.Execute(os.Stdout, user)
+	if err != nil {
+		panic(err)
+	}
+	
+
+}
--- main.go
+++ main.go
@@ -16,10 +16,28 @@ func contact(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-type", "text/html")
 	fmt.Fprint(w, "To get in touch please send an email to <a href=\"mailto:myriam.azzouz@holbertonschool.com\"> support </a>.")
 }
+
+func faq(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-type", "text/html")
+	fmt.Fprint(w, "<h1>FAQ</h1>")
+	fmt.Fprint(w, "1/ Who should Icontact in case I got 404 ?")
+}
+func notfound(w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(http.StatusNotFound)
+	w.Header().Set("Content-type", "text/html")
+	fmt.Fprint(w, "OOps 404 ! ")
+
+}
+
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/FAQ", faq)
+	// the handler function is an actual type
+
+	r.NotFoundHandler = http.HandlerFunc(notfound)
 	http.ListenAndServe(":3000", r)
 	    
 }
